fc4ef474979bedc30fad0a7a1e7766719a5f7553
CamaKot <92314609+CamaKot@users.noreply.github.com>
2022-12-15 14:52:03 +0300

    cv show

--- PedestrianDetecting/DepthMap.py
+++ PedestrianDetecting/DepthMap.py
@@ -2,15 +2,91 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-imgL = cv.imread("images/image_00005125_0.png", 0)
-imgR = cv.imread("images/image_00005126_0.png", 0)
+
+
+imgL = cv.imread("E:\Study\Pedestrian detecting\Pedestrian-detecting\DataSet\lp-left\image_00005205_0.png", 0)
+imgR = cv.imread("E:\Study\Pedestrian detecting\Pedestrian-detecting\DataSet\lp-left\image_00005206_0.png", 0)
+obj_path = 'E:\Study\Pedestrian detecting\Pedestrian-detecting\DataSet\maps-loewenplatz\maps-sfm-avg-jan2010\path.txt'
+K1 = np.array([[801.25700, 0., 325.05589], [0.,  802.24324, 243.29150], [0., 0., 1.]])
+K2 = np.array([[804.81163, 0., 335.67663], [0.,  805.19672, 232.42614], [0., 0., 1.]])
+rad1 = np.array([-0.22813,   0.16483])
+rad2 = np.array([-0.21484,   0.10025])
+dist1 = np.array([-0.22813,   0.16483,  0., 0.])
+dist2 = np.array([-0.21484,   0.10025,  0., 0.])
+
+Rt1 = np.array([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])
+Rt2 = np.array([[0.999923235343999,  -0.005513666805298,   0.011096075772572],
+       [ 0.005431036122504,   0.999957401012593,   0.007463243716854],
+       [-0.011136752930121,  -0.007402407615178,  0.999910584550275]])
+
+C1 = np.array([0., 0., 0.])
+C2 = np.array([-0.6192593383286313, -0.0007428029644263, -0.0142760640908179])
+obj_pnt = []
+
+
+def getObj():
+    f = open(obj_path, 'r', )
+    for line in f:
+
+        obj_pnt.append(line.strip().split(sep=' '))
+    f.close()
+    for line in obj_pnt:
+        for i in range(len(line)):
+            line[i] = float(line[i])
+
 
 #imgL = cv.cvtColor(imgL, imgL,  cv.COLOR_BGR2RGB)
+def getDepthmap(imgL, imgR):
+    # obj_pnts = np.array(obj_pnt)
+    # #left calib
+    # hl, wl = imgL.shape[:2]
+    # new_mtxL, roiL = cv.getOptimalNewCameraMatrix(K1, dist1, (wl, hl),1,(wl,hl))
+    # #imageL = new_mtxL#imgL
+    #
+    # #right calib
+    # hr, wr = imgR.shape[:2]
+    # new_mtxR, roiR = cv.getOptimalNewCameraMatrix(K2, dist2, (wr, hr), 1, (wr, hr))
+    # #imageR = new_mtxR#imgR
+    #
+    # flags = 0
+    # flags |= cv.CALIB_FIX_INTRINSIC
+    # # Here we fix the intrinsic camara matrixes so that only Rot, Trns, Emat and Fmat are calculated.
+    # # Hence intrinsic parameters are the same
+    #
+    #
+    # criteria_stereo = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # retS, new_mtxL, distL, new_mtxR, distR, Rot, Trns, Emat, Fmat = cv.stereoCalibrate(obj_pnts, imgL, imgR,
+    #                                                                                    new_mtxL, dist1, new_mtxR,
+    #                                                                                    dist2, imgL.shape[::-1],
+    #                                                                                    criteria_stereo, flags)
+    #
+    # rectify_scale = 1
+    # rect_l, rect_r, proj_mat_l, proj_mat_r, Q, roiL, roiR = cv.stereoRectify(new_mtxL, dist1, new_mtxR, dist2,
+    #                                                                           imgL.shape[::-1], Rot, Trns,
+    #                                                                           rectify_scale, (0, 0))
+    #
+    # #Пропарсить файлы и вытащить от туда значения
+    # imageL = cv.initUndistortRectifyMap(new_mtxL, distL, rect_l, proj_mat_l, imgL.shape[::-1], cv.CV_8UC1)
+    # imageR = cv.initUndistortRectifyMap(new_mtxR, distR, rect_l, proj_mat_r, imgR.shape[::-1], cv.CV_8UC1)
+    imageL = imgL
+    imageR = imgR
 
-stereo = cv.StereoBM_create(numDisparities=16, blockSize=15)
-disparity = stereo.compute(imgL, imgR)
-plt.imshow(disparity, 'gray')
-cv.imshow('Left', imgL)
-cv.imshow('Right', imgR)
-plt.show()
-cv.waitKey()
+    stereo = cv.StereoBM_create(numDisparities=16, blockSize=15)
+    disparity = stereo.compute(imageL, imageR)
+    return disparity/255
+
+
+
+
+def imgCVshow(disparity):
+    plt.imshow(disparity, 'gray')
+    cv.imshow('Left', imgL)
+    cv.imshow('Right', imgR)
+    plt.show()
+    cv.waitKey()
+
+def main():
+    imgCVshow(getDepthmap(imgL, imgR))
+
+if __name__ == '__main__':
+    main()
--- PedestrianDetecting/main.py
+++ PedestrianDetecting/main.py
@@ -1,16 +1,135 @@
-# This is a sample Python script.
+import sys
+import numpy as np
+import random
+import matplotlib
+import glob
+import cv2 as cv
+from PIL import Image
+from matplotlib import pyplot as plt
+from PyQt5 import QtCore, QtWidgets, uic, QtGui
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+from PyQt5.QtGui import QPixmap
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+import DepthMap as dpm
+matplotlib.use('Qt5Agg')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+imgL_list = []
+imgR_list = []
+maps = []
+bound_rect = []
+
+class MplCanvas(FigureCanvasQTAgg):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width,height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        uic.loadUi('MainWindow.ui', self)
+        self.title = "Pedestrian detecting"
+        self.setWindowTitle(self.title)
+        self.start_stop_pushButton.clicked.connect(self.play_pushed)
+
+        dpm.getObj()
+
+        self.isDraw = True
+        self.isPlaying = False
+        self.show()
+        self.current_img = 0
+        self.fps = 14
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.update)
+        #self.update()
+        #self.play_pushed()
 
 
-# Press the green button in the gutter to run the script.
+    def play_pushed(self):
+        if self.isPlaying:
+            self.stop()
+            self.start_stop_pushButton.setText('START')
+            self.isPLaying = False
+        else:
+            self.start()
+            self.start_stop_pushButton.setText('STOP')
+            self.isPLaying = True
+
+
+    def draw(self, img, disp, ):
+        pass
+
+
+    def update(self):
+        if self.current_img+1 >= len(imgL_list)-2:
+            self.current_img = 0
+
+        #self.current_img = 305
+        pixmapL = cv.imread(imgL_list[self.current_img], cv.CV_8UC1)
+        #pixmapR = cv.imread(imgR_list[self.current_img], cv.CV_8UC1)
+        pixmapR = cv.imread(imgL_list[self.current_img+1], cv.CV_8UC1)
+
+        map = cv.imread(maps[self.current_img])
+
+
+        pixmap = dpm.getDepthmap(pixmapL, pixmapR)
+
+        # if self.isDraw:
+        #     self.draw()
+
+        cv.imshow('disp', pixmap)
+        cv.imshow('Main', pixmapL)
+        #pix = QtGui.QImage(pixmap.data, pixmap.shape[1], pixmap.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped()
+        #self.pic_label.setPixmap(QPixmap.fromImage(pix))
+        print(f'{self.current_img}')
+        self.current_img += 1
+
+
+
+    def start(self):
+        self.timer.setInterval(int(1000/self.fps))
+        self.timer.start()
+
+    def stop(self):
+        self.timer.stop()
+
+
+
+
+
+def PhotoBank():
+    global imgL_list
+    global imgR_list
+    global maps
+    pathL = 'E:\Study\Pedestrian detecting\Pedestrian-detecting\DataSet\lp-left\*'#"E:/Study/Pedestrian detecting/DataSet/lp-left/*"
+    pathR = 'E:/Study/Pedestrian detecting/Pedestrian-detecting/DataSet/lp-right/*'#"E:/Study/Pedestrian detecting/DataSet/lp-right/*"
+    mapPath = 'E:\Study\Pedestrian detecting\Pedestrian-detecting\DataSet\maps-loewenplatz\maps-sfm-avg-jan2010\*'
+    bound_rect_path = 'E:\Study\Pedestrian detecting\Pedestrian-detecting\DataSet\lp-annot.idl'
+    bound = glob.glob(bound_rect_path)
+
+    with open(bound_rect_path, 'r') as f:
+        f.read().splitlines()
+    print(f)
+    # for filename in glob.glob(pathL):  # assuming gif
+    #     im = Image.open(filename)
+    #     imgL_list.append(im)
+    imgL_list = glob.glob(pathL)
+    imgR_list = glob.glob(pathR)
+    maps = glob.glob(mapPath)
+
+
+def main():
+    PhotoBank()
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+
+    sys.exit(app.exec_())
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
